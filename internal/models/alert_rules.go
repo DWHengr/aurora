ddb49991a3da615b399e1db46b3483c7556596ce
henamdu <duweihengmr@163.com>
2022-08-16 15:40:30 +0800

    fix: modify rules entity

--- internal/models/alert_rules.go
+++ internal/models/alert_rules.go
@@ -5,15 +5,17 @@ import "gorm.io/gorm"
 type AlertRules struct {
 	BaseModel
 
-	Name          string `json:"name"`
-	AlertObject   string `json:"alert_object"`
-	Rules         string `json:"rules"`
-	RulesStatus   string `json:"rules_status"`
-	Webhook       string `json:"webhook"`
-	Persistent    string `json:"persistent"`
-	AlertInterval string `json:"alert_interval"`
-	StoreInterval string `json:"store_interval"`
-	Description   string `json:"description"`
+	Name            string `json:"name"`
+	AlertObject     string `json:"alertObject"`
+	Rules           string `json:"rules"`
+	RulesStatus     string `json:"rulesStatus"`
+	Severity        string `json:"severity"`
+	Webhook         string `json:"webhook"`
+	AlertSilencesId string `json:"alertSilencesId"`
+	Persistent      string `json:"persistent"`
+	AlertInterval   string `json:"alertInterval"`
+	StoreInterval   string `json:"storeInterval"`
+	Description     string `json:"description"`
 }
 
 type AlertRulesRepo interface {
