515d0ff681ee74af62e9c5ee2baa5f9441706338
henamdu <duweihengmr@163.com>
2022-08-16 15:40:46 +0800

    fix: modify metrics entity

--- internal/models/alert_metrics.go
+++ internal/models/alert_metrics.go
@@ -7,6 +7,7 @@ type AlertMetrics struct {
 
 	Name        string `json:"name"`
 	Type        string `json:"type"`
+	Expression  string `json:"expression"`
 	Unit        string `json:"unit"`
 	Operator    string `json:"operator"`
 	Description string `json:"description"`
