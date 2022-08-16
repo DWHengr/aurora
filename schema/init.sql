63c8445470f97e9b14529f2455a24772b8b72550
henamdu <duweihengmr@163.com>
2022-08-16 15:35:25 +0800

    fix: modify sql structure

--- schema/init.sql
+++ schema/init.sql
@@ -7,7 +7,7 @@ CREATE TABLE `alert_metrics`
     `name`        varchar(200) NOT NULL COMMENT '指标名称',
     `type`        varchar(50)  NOT NULL COMMENT '指标类型',
     `expression`  varchar(500) NOT NULL COMMENT '表达式',
-    `unit`        varchar(100)  NOT NULL COMMENT '单位',
+    `unit`        varchar(100) NOT NULL COMMENT '单位',
     `operator`    varchar(22)  NOT NULL COMMENT '操作符',
     `description` varchar(500) COMMENT '备注',
     PRIMARY KEY (`id`)
@@ -17,18 +17,18 @@ CREATE TABLE `alert_metrics`
 DROP TABLE if EXISTS `alert_rules`;
 CREATE TABLE `alert_rules`
 (
-    `id`             varchar(40)  NOT NULL,
-    `name`           varchar(200) NOT NULL COMMENT '规则名称',
-    `alert_object`   text         NOT NULL COMMENT '告警对象,json{key:value}',
-    `rules`          text         NOT NULL COMMENT '告警规则,json{metric:metricId,statistics:statistics,operator:operator,value:value}',
-    `rules_status`   int          DEFAULT 1 COMMENT '0:禁用,1:启用',
-    `severity`       varchar(64)  DEFAULT NULL COMMENT '告警等级,hint,minor,importance,urgency',
-    `webhook`        varchar(200) DEFAULT NULL COMMENT '回调接口',
-    `alert_silences` varchar(40)  DEFAULT NULL COMMENT '告警静默id',
-    `persistent`     varchar(64)  DEFAULT NULL COMMENT '持续时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不告警',
-    `alert_interval` varchar(64)  DEFAULT NULL COMMENT '告警间隔时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不告警',
-    `store_interval` varchar(64)  DEFAULT NULL COMMENT '存储间隔时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不存储',
-    `description`    varchar(500) COMMENT '备注',
+    `id`                varchar(40)  NOT NULL,
+    `name`              varchar(200) NOT NULL COMMENT '规则名称',
+    `alert_object`      text         NOT NULL COMMENT '告警对象,json{key:value}',
+    `rules`             text         NOT NULL COMMENT '告警规则,json{metric:metricId,statistics:statistics,operator:operator,value:value}',
+    `rules_status`      int          DEFAULT 1 COMMENT '0:禁用,1:启用',
+    `severity`          varchar(64)  DEFAULT NULL COMMENT '告警等级,hint,minor,importance,urgency',
+    `webhook`           varchar(200) DEFAULT NULL COMMENT '回调接口',
+    `alert_silences_id` varchar(40)  DEFAULT NULL COMMENT '告警静默id',
+    `persistent`        varchar(64)  DEFAULT NULL COMMENT '持续时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不告警',
+    `alert_interval`    varchar(64)  DEFAULT NULL COMMENT '告警间隔时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不告警',
+    `store_interval`    varchar(64)  DEFAULT NULL COMMENT '存储间隔时间（默认为s）,单位:ms(毫秒),s（秒）,m(分),h(时),d(天),如果为null表示不存储',
+    `description`       varchar(500) COMMENT '备注',
     PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='告警规则表' row_format=dynamic;
 
