a74eb5ced331f7bad3c3a17d22bcf71fbc739569
henamdu <duweihengmr@163.com>
2022-08-18 14:04:21 +0800

    fix: modify prometheus message struct

--- api/prometheus.go
+++ api/prometheus.go
@@ -13,13 +13,30 @@ func NewPrometheus() *Prometheus {
 	return &Prometheus{}
 }
 
+type PrometheusReq struct {
+	Labels      map[string]string `json:"labels"`
+	Annotations map[string]string `json:"annotations"`
+}
+
 func (p *Prometheus) Alerts(c *gin.Context) {
-	msgs := &[]*alertcore.AlertMessage{}
-	if err := c.ShouldBind(msgs); err != nil {
+	reqs := &[]*PrometheusReq{}
+	if err := c.ShouldBind(reqs); err != nil {
 		c.AbortWithError(http.StatusInternalServerError, err)
 		return
 	}
-	for _, msg := range *msgs {
+	for _, req := range *reqs {
+		msg := &alertcore.AlertMessage{
+			Name:      req.Labels["alertname"],
+			Value:     req.Annotations["value"],
+			Summary:   req.Annotations["summary"],
+			Attribute: make(map[string]interface{}),
+		}
+		for k, v := range req.Labels {
+			msg.Attribute[k] = v
+		}
+		for k, v := range req.Annotations {
+			msg.Attribute[k] = v
+		}
 		alertcore.AlertInstance.Receive(msg)
 	}
 }
