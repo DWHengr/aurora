96e77c05617a7a361d41840ae31254d46f7fba30
henamdu <duweihengmr@163.com>
2022-08-23 21:57:15 +0800

    feat: rule struct modify

--- internal/models/alert_rules.go
+++ internal/models/alert_rules.go
@@ -1,13 +1,26 @@
 package models
 
-import "gorm.io/gorm"
+import (
+	"database/sql/driver"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"gorm.io/gorm"
+)
+
+type rule struct {
+	Metric     string `json:"metric"`
+	Statistics string `json:"statistics"`
+	Operator   string `json:"operator"`
+	AlertValue string `json:"alertValue"`
+}
 
 type AlertRules struct {
 	BaseModel
 
 	Name            string `json:"name"`
 	AlertObject     string `json:"alertObject"`
-	Rules           string `json:"rules"`
+	Rules           rule   `json:"rules"`
 	RulesStatus     string `json:"rulesStatus"`
 	Severity        string `json:"severity"`
 	Webhook         string `json:"webhook"`
@@ -16,6 +29,23 @@ type AlertRules struct {
 	AlertInterval   string `json:"alertInterval"`
 	StoreInterval   string `json:"storeInterval"`
 	Description     string `json:"description"`
+}
+
+func (r *rule) Scan(value interface{}) error {
+	bytes, ok := value.([]byte)
+	if !ok {
+		return errors.New(fmt.Sprint("Failed to unmarshal JSONB value:", value))
+	}
+	result := rule{}
+	err := json.Unmarshal(bytes, &result)
+	*r = result
+	return err
+}
+
+func (r rule) Value() (driver.Value, error) {
+	jsonByte, err := json.Marshal(r)
+	jsonStr := string(jsonByte)
+	return jsonStr, err
 }
 
 type AlertRulesRepo interface {
